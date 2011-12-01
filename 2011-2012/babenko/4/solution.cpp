9e6e7996db42758d04360d27f71bf68cf3864438
Stanislav Pak <abss.pakso@gmail.com>
2011-12-02 02:33:09 +0400

    Redesigned solution

--- 2011-2012/babenko/4/solution.cpp
+++ 2011-2012/babenko/4/solution.cpp
@@ -17,24 +17,13 @@ const size_t UNDEFINED_INDEX = std::numeric_limits<size_t>::max();
 template <class Heap>
 class Element
 {
-    public:
-
+public:
     Element() : Element(0) { }
 
     explicit Element(int key, Heap* heap = NULL,
-            Element<Heap>* source = NULL,
-            size_t index = UNDEFINED_INDEX) :
-        key_(key), heap_(heap), source_(source), index_(index)
+                     size_t index = UNDEFINED_INDEX) :
+        key_(key), heap_(heap), index_(index)
     { }
-
-    template <class SimilarElement>
-    Element(const SimilarElement& element)
-    {
-        key_ = element.key();
-        heap_ = reinterpret_cast<Heap*>(element.heap());
-        source_ = reinterpret_cast<Element<Heap>*>(element.source());
-        index_ = element.index();
-    }
 
     int key() const { return key_; }
 
@@ -44,24 +33,17 @@ class Element
 
     size_t index() { return index_; }
 
-    void setIndex(size_t newIndex) { index_ = newIndex; }
+    void setIndex(size_t index) { index_ = index; }
 
     Heap* heap() const { return heap_; }
 
     Heap* heap() { return heap_; }
 
-    Element<Heap>* source() const { return source_; }
-
-    Element<Heap>* source() { return source_; }
-
     void setHeap(Heap* heap) { heap_ = heap; }
 
-    void setSource(Element<Heap>* source) { source_ = source; }
-
-    private:
+private:
     int key_;
     Heap* heap_;
-    Element<Heap>* source_;
     size_t index_;
 };
 
@@ -69,8 +51,8 @@ template <class Element>
 class HeapElementComparator
 {
     public:
-    virtual bool operator()(const Element& first,
-                            const Element& second) const
+    virtual bool operator()(const Element* first,
+                            const Element* second) const
     {
         throw std::runtime_error("Operation is not supported");
     }
@@ -78,78 +60,57 @@ class HeapElementComparator
     ~HeapElementComparator() { }
 };
 
-template <class Element>
-class HeapElementLess : public HeapElementComparator<Element>
-{
-    public:
-    virtual bool operator()(const Element& first,
-                            const Element& second) const
-    {
-        return first.key() < second.key();
-    }
-};
-
-template <class Element>
-class HeapElementGreater : public HeapElementComparator<Element>
-{
-    public:
-    virtual bool operator()(const Element& first,
-                            const Element& second) const
-    {
-        return first.key() > second.key();
-    }
-};
-
 // This class does not own elements
 template <template <class> class Element, template <class> class Comparator>
 class BinaryHeap
 {
-    public:
-
+public:
     typedef Element<BinaryHeap> HeapElement;
+    typedef Comparator<HeapElement> HeapElementComparator;
 
-    void push(HeapElement element)
+    BinaryHeap(const Comparator<HeapElement>& comparator)
+        : comparator_(comparator) { }
+
+    void push(HeapElement* element)
     {
-        element.setHeap(this);
-        element.setIndex(heap_.size());
+        element->setHeap(this);
+        element->setIndex(heap_.size());
         heap_.push_back(element);
         siftUp(heap_.size() - 1);
     }
 
-    HeapElement pop()
+    HeapElement* pop()
     {
         require(!heap_.empty(), "Cannot get min element from empty heap");
 
-        HeapElement result = *heap_.begin();
+        HeapElement* result = *heap_.begin();
         swap(*heap_.begin(), heap_.back());
         heap_.pop_back();
 
         siftDown(0);
 
-        result.setIndex(UNDEFINED_INDEX);
-        result.setHeap(NULL);
+        result->setIndex(UNDEFINED_INDEX);
+        result->setHeap(NULL);
 
         return result;
     }
 
-    HeapElement remove(size_t index)
+    void remove(size_t index)
     {
         require(index < heap_.size(), "Index is out of range");
 
         swap(heap_[index], heap_.back());
 
-        HeapElement result = heap_.back();
+        HeapElement* result = heap_.back();
         heap_.pop_back();
 
         siftDown(index);
-
-        return result;
     }
 
     const HeapElement& top() const
     {
         require(!heap_.empty(), "Cannot get min element from empty heap");
-        return *heap_.begin();
+        return **heap_.begin();
     }
 
     bool empty() const { return heap_.size() == 0; }
@@ -158,15 +119,25 @@ class BinaryHeap
 
     size_t size() { return heap_.size(); }
 
-    private:
+    friend std::ostream& operator<<(std::ostream& os, const BinaryHeap& heap)
+    {
+        os << "Heap : ";
+        for (size_t i = 0; i < heap.heap_.size(); ++i) {
+            os << " " << heap.heap_[i]->key();
+        }
+        return os;
+    }
 
+private:
     void siftUp(size_t position)
     {
+        require(position < heap_.size(), "position is out of range");
+
         while (position > 0 &&
                comparator_(heap_[parent(position)], heap_[position])) {
 
-            HeapElement& current = heap_[position];
-            HeapElement& next = heap_[parent(position)];
+            HeapElement*& current = heap_[position];
+            HeapElement*& next = heap_[parent(position)];
 
             swap(current, next);
 
@@ -182,16 +153,16 @@ class BinaryHeap
                canBeSifted) {
 
             size_t nextPosition = leftChild(position);
-            HeapElement& greatest = heap_[nextPosition];
 
-
-            if (rightChild(position) < heap_.size() &&
-                comparator_(greatest, heap_[rightChild(position)])) {
-                nextPosition = rightChild(position);
-                greatest = heap_[nextPosition];
+            if (nextPosition + 1 < heap_.size() &&
+                comparator_(heap_[nextPosition],
+                            heap_[nextPosition + 1])) {
+                ++nextPosition;
             }
 
-            HeapElement& current = heap_[position];
+            HeapElement*& current = heap_[position];
+            HeapElement*& greatest = heap_[nextPosition];
+
             if (comparator_(current, greatest)) {
                 swap(current, greatest);
             }
@@ -202,11 +173,11 @@ class BinaryHeap
         }
     }
 
-    void swap(HeapElement& first, HeapElement& second) const
+    void swap(HeapElement*& first, HeapElement*& second) const
     {
-        size_t index = first.index();
-        first.setIndex(second.index());
-        second.setIndex(index);
+        size_t index = first->index();
+        first->setIndex(second->index());
+        second->setIndex(index);
 
         std::swap(first, second);
     }
@@ -217,17 +188,38 @@ class BinaryHeap
 
     size_t rightChild(size_t pos) const { return (pos + 1) << 1; }
 
-    typename std::vector<HeapElement> heap_;
-    Comparator<HeapElement> comparator_;
+    typename std::vector<HeapElement*> heap_;
+    const Comparator<HeapElement>& comparator_;
 };
 
 typedef BinaryHeap<Element, HeapElementComparator> CommonHeap;
 typedef CommonHeap::HeapElement HeapElement;
-typedef Element<CommonHeap> CommonElement;
+
+class HeapElementLess : public CommonHeap::HeapElementComparator
+{
+    public:
+    virtual bool operator()(const HeapElement* first,
+                            const HeapElement* second) const
+    {
+        return first->key() < second->key();
+    }
+};
+
+class HeapElementGreater : public CommonHeap::HeapElementComparator
+{
+    public:
+    virtual bool operator()(const HeapElement* first,
+                            const HeapElement* second) const
+    {
+        return first->key() > second->key();
+    }
+};
+
+typedef std::vector<HeapElement> HeapElements;
 
 void processInputData(size_t* kthOrderStatistic,
                       std::string* operations,
-                      std::vector<HeapElement>* elements)
+                      HeapElements* elements)
 {
     size_t numElements, numOperations;
     std::cin >> numElements >> numOperations >> *kthOrderStatistic;
@@ -245,44 +237,40 @@ void processInputData(size_t* kthOrderStatistic,
 
 void processSolution(size_t kthOrderStatistic,
                      const std::string& operations,
-                     std::vector<HeapElement> elements)
+                     HeapElements elements)
 {
-    typedef BinaryHeap<Element, HeapElementLess> HeapLess;
-    typedef BinaryHeap<Element, HeapElementGreater> HeapGreater;
+    HeapElementLess less;
+    HeapElementGreater greater;
 
-    // initialize sources to update elements data
-    for (size_t index = 0; index < elements.size(); ++index) {
-        elements[index].setSource(&elements[index]);
-    }
-
-    HeapLess kHeap;
-    HeapGreater overflowHeap;
+    CommonHeap kHeap(less);
+    CommonHeap overflowHeap(greater);
 
     // initial positions of pointers, the right one is exclusive
     size_t pointerL = 0;
     size_t pointerR = pointerL + 1;
 
     // push the first element to kHeap, as kthOrderStatistic > 0
-    kHeap.push(static_cast<HeapLess::HeapElement>(*elements.begin()));
+    kHeap.push(&*elements.begin());
+
+    // std::cerr << kHeap << std::endl;
+    // std::cerr << overflowHeap << std::endl;
 
     for (size_t operationIndex = 0; operationIndex < operations.size();
                 ++operationIndex) {
         bool needTransfer = false;
         if (operations[operationIndex] == 'L') {
 
-            HeapElement element = elements[pointerL++];
+            const HeapElement& element = elements[pointerL++];
             CommonHeap* heap = element.heap();
             heap->remove(element.index());
             if (kHeap.size() < kthOrderStatistic &&
                 !overflowHeap.empty()) {
                 needTransfer = true;
             }
-
         }
         else if (operations[operationIndex] == 'R') {
 
-            overflowHeap.push(static_cast<HeapGreater::HeapElement>(
-                                                    elements[pointerR++]));
+            overflowHeap.push(&elements[pointerR++]);
 
             if (kHeap.size() < kthOrderStatistic ||
                     // quite dangerous piece of code, as it makes additional
@@ -295,10 +283,14 @@ void processSolution(size_t kthOrderStatistic,
             throw std::runtime_error("Unknown operation");
         }
         if (needTransfer) {
-            HeapElement greatest = kHeap.pop();
             kHeap.push(overflowHeap.pop());
-            overflowHeap.push(greatest);
+            if (kHeap.size() > kthOrderStatistic) {
+                overflowHeap.push(kHeap.pop());
+            }
         }
+
+        // std::cerr << kHeap << std::endl;
+        // std::cerr << overflowHeap << std::endl;
 
         // output kth order statistic if it exists
         if (kHeap.size() == kthOrderStatistic) {
@@ -310,16 +302,45 @@ void processSolution(size_t kthOrderStatistic,
     }
 }
 
+void test()
+{
+    HeapElementLess less;
+    CommonHeap heap(less);
+    std::vector<HeapElement> elements;
+    for (size_t i = 0; i < 10; ++i) {
+        elements.push_back(HeapElement(i));
+    }
+    for (size_t i = 0; i < 5; ++i) {
+        heap.push(&elements[i]);
+    }
+    using std::cerr;
+    using std::endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    for (size_t i = 5; i < 10; ++i) {
+        heap.push(&elements[i]);
+    }
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    cerr << heap.pop()->key() << endl;
+    exit(0);
+}
+
 int main()
 {
     std::ios_base::sync_with_stdio(false);
+    //test();
 
     std::string operations;
     size_t kthOrderStatistic;
     std::vector<HeapElement> elements;
-
     processInputData(&kthOrderStatistic, &operations, &elements);
 
     processSolution(kthOrderStatistic, operations, elements);
-
+    return 0;
 }
