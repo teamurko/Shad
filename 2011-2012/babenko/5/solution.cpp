47dd8b0fb6fe0f5220a609278119e53fd0480dc3
Stanislav Pak <abss.pakso@gmail.com>
2011-12-20 21:17:42 +0400

    Design fixes

--- 2011-2012/babenko/5/solution.cpp
+++ 2011-2012/babenko/5/solution.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <limits>
 
+enum {EMPTY_PLACE = -2, FULL_PARKING, PLACE_FREED};
+
 const size_t UNDEFINED_INDEX = std::numeric_limits<size_t>::max();
 
 class FenwickTree
@@ -42,10 +44,10 @@ private:
     std::vector<int> tree_;
 };
 
-void readData(size_t* parkingSize, std::vector<int>* operations)
+void readData(size_t& parkingSize, std::vector<int>* operations)
 {
     size_t operationsNumber;
-    std::cin >> *parkingSize >> operationsNumber;
+    std::cin >> parkingSize >> operationsNumber;
     operations->resize(operationsNumber);
     for (size_t index = 0; index < operations->size(); ++index) {
         char sign;
@@ -78,42 +80,60 @@ size_t findFirstEmpty(const FenwickTree& tree, size_t begin, size_t end)
     }
 }
 
-void solve(size_t parkingSize, const std::vector<int>& operations)
+void processArrived(int operation, size_t parkingSize,
+                    FenwickTree* tree, std::vector<int>* result)
+{
+    int position = operation - 1;
+    size_t place = findFirstEmpty(*tree, position,
+                               position + parkingSize);
+    if (place != UNDEFINED_INDEX) {
+        tree->update(place, 1);
+        if (place < parkingSize) {
+            tree->update(place + parkingSize, 1);
+        }
+        else {
+            tree->update(place - parkingSize, 1);
+        }
+        result->push_back(place % parkingSize + 1);
+    }
+    else {
+        result->push_back(FULL_PARKING);
+    }
+}
+
+void processLeaving(int operation, size_t parkingSize,
+                    FenwickTree* tree, std::vector<int>* result)
+{
+    int position = -operation - 1;
+    if ((*tree)(position, position + 1) > 0) {
+        tree->update(position, -1);
+        tree->update(position + parkingSize, -1);
+        result->push_back(PLACE_FREED);
+    }
+    else {
+        result->push_back(EMPTY_PLACE);
+    }
+}
+
+void solve(size_t parkingSize, const std::vector<int>& operations,
+           std::vector<int>* result)
 {
     FenwickTree tree(2 * parkingSize);
     for (size_t index = 0; index < operations.size(); ++index) {
         int operation = operations[index];
-        // someone arrived
         if (operation > 0) {
-            int position = operation - 1;
-            size_t place = findFirstEmpty(tree, position,
-                                       position + parkingSize);
-            if (place != UNDEFINED_INDEX) {
-                tree.update(place, 1);
-                if (place < parkingSize) {
-                    tree.update(place + parkingSize, 1);
-                }
-                else {
-                    tree.update(place - parkingSize, 1);
-                }
-                std::cout << place % parkingSize + 1 << std::endl;
-            }
-            else {
-                std::cout << -1 << std::endl;
-            }
+            processArrived(operation, parkingSize, &tree, result);
         }
-        // someone leaves
         else {
-            int position = -operation - 1;
-            if (tree(position, position + 1) > 0) {
-                tree.update(position, -1);
-                tree.update(position + parkingSize, -1);
-                std::cout << 0 << std::endl;
-            }
-            else {
-                std::cout << -2 << std::endl;
-            }
+            processLeaving(operation, parkingSize, &tree, result);
         }
+    }
+}
+
+void outputData(const std::vector<int>& responses)
+{
+    for (size_t index = 0; index < responses.size(); ++index) {
+        std::cout << responses[index] << std::endl;
     }
 }
 
@@ -123,7 +143,9 @@ int main()
 
     size_t parkingSize;
     std::vector<int> operations;
-    readData(&parkingSize, &operations);
-    solve(parkingSize, operations);
+    readData(parkingSize, &operations);
+    std::vector<int> queryResponses;
+    solve(parkingSize, operations, &queryResponses);
+    outputData(queryResponses);
     return 0;
 }
